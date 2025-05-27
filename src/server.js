5e8e7cf85f428862f6217be406184ea0a35a006f
Vladislav Mazikov <vladislavmazikov@yandex.ru>
2025-05-27 21:22:54 +0700

    Поправил файл сервера

--- src/server.js
+++ src/server.js
@@ -2,4 +2,5 @@ import app from './app.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log('API v2 listening on ' + PORT));
+app.listen(PORT, '0.0.0.0', () => console.log('API v2 listening on ' + PORT));
+
