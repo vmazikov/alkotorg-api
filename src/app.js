178dd2431143dc7204a559f19fa2e471b1d982b1
Vladislav Mazikov <vladislavmazikov@yandex.ru>
2025-05-27 23:20:44 +0700

    Поправил лимиты

--- src/app.js
+++ src/app.js
@@ -46,7 +46,11 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({
+  extended: true,
+  limit: '50mb'
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
